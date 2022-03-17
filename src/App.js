91dc7fae2b364903ad16d6a2eae0ed15ede36ef2
Samdejii <samdejii97@gmail.com>
2022-03-17 11:22:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+//import logo from './logo.svg';
+import './App.css';
+import RouterPage from './pages/RouterPage';
+
+function App() {
+  return (
+    <RouterPage />
+  );
+}
+
+export default App;
